a037ef2d71f42a341ccd758f2b80d9ad214855e9
John-Muriu <johnmuriw@gmail.com>
2020-08-19 15:53:56 +0300

    define ball collision detection

--- main.js
+++ main.js
@@ -58,19 +58,19 @@ Ball.prototype.update = function () {
 
 // // define ball collision detection
 
-// Ball.prototype.collisionDetect = function () {
-//     for (let j = 0; j < balls.length; j++) {
-//         if (!(this === balls[j])) {
-//             const dx = this.x - balls[j].x;
-//             const dy = this.y - balls[j].y;
-//             const distance = Math.sqrt(dx * dx + dy * dy);
+Ball.prototype.collisionDetect = function () {
+    for (let j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            const dx = this.x - balls[j].x;
+            const dy = this.y - balls[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
 
-//             if (distance < this.size + balls[j].size) {
-//                 balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
-//             }
-//         }
-//     }
-// };
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
+    }
+};
 
 // // define array to store balls and populate it
 
