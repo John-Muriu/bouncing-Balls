eea78f6780f56bbddae1ec6550dc52c98dc5de10
John-Muriu <johnmuriw@gmail.com>
2020-08-19 15:51:26 +0300

    defining the ball constructor

--- main.js
+++ main.js
@@ -15,14 +15,14 @@ function random(min, max) {
 
 // // // define Ball constructor
 
-// // function Ball(x, y, velX, velY, color, size) {
-// //     this.x = x;
-// //     this.y = y;
-//     this.velX = velX;
-//     this.velY = velY;
-//     this.color = color;
-//     this.size = size;
-// }
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+}
 
 // // define ball draw method
 
